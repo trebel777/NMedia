5f83569b5ef601ce4bba862d7d958f67348abb65
trebel777 <trebel777@gmail.com>
2022-11-29 13:14:37 +0300

    cancelEdit

--- app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -2,9 +2,14 @@ package ru.netology.nmedia.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
+import ru.netology.nmedia.R
+import ru.netology.nmedia.adapter.OnInteractionListener
 import ru.netology.nmedia.adapter.PostsAdapter
 import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.util.AndroidUtils
 import ru.netology.nmedia.viewmodel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
@@ -17,14 +22,59 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(binding.root)
         val viewModel: PostViewModel by viewModels()
-        val adapter = PostsAdapter( {
-            viewModel.likeById(it.id)
-        },{
-            viewModel.replyById(it.id)
+        val adapter = PostsAdapter(object : OnInteractionListener {
+            override fun onEdit(post: Post) {
+                viewModel.edit(post)
+            }
+
+            override fun onLike(post: Post) {
+                viewModel.likeById(post.id)
+            }
+
+            override fun onReply(post: Post) {
+                viewModel.replyById(post.id)
+            }
+
+            override fun onRemove(post: Post) {
+                viewModel.removeById(post.id)
+            }
         })
         binding.list.adapter = adapter
         viewModel.data.observe(this) { posts ->
-            adapter.list = posts
+            adapter.submitList(posts)
+        }
+        viewModel.edited.observe(this) { post ->
+            if (post.id == 0L) {
+                return@observe
+            }
+            with(binding.content) {
+                requestFocus()
+                setText(post.content)
+            }
+        }
+        binding.save.setOnClickListener {
+            with(binding.content) {
+                if (text.isNullOrBlank()) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        context.getString(R.string.error_empty_content),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return@setOnClickListener
+                }
+                viewModel.changeContent(text.toString())
+                viewModel.save()
+                setText("")
+                clearFocus()
+                AndroidUtils.hideKeyboard(this)
+            }
+        }
+        binding.cancelEdit.setOnClickListener {
+            with(binding.content) {
+                setText("")
+                clearFocus()
+                AndroidUtils.hideKeyboard(this)
+            }
         }
     }
 }
--- app/src/main/java/ru/netology/nmedia/adapter/PostsAdapter.kt
+++ app/src/main/java/ru/netology/nmedia/adapter/PostsAdapter.kt
@@ -2,42 +2,44 @@ package ru.netology.nmedia.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.PopupMenu
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import ru.netology.nmedia.R
 import ru.netology.nmedia.activity.PostService
 import ru.netology.nmedia.databinding.CardPostBinding
 import ru.netology.nmedia.dto.Post
 
-typealias OnLikeListener = (post: Post) -> Unit
-typealias OnReplyListener = (post: Post) -> Unit
+interface OnInteractionListener{
+    fun onLike(post: Post) {}
+    fun onEdit(post: Post) {}
+    fun onReply(post: Post) {}
+    fun onRemove(post: Post) {}
+}
 var ps = PostService()
 
-class PostsAdapter(private val onLikeListener: OnLikeListener,private val onReplyListener: OnReplyListener) : RecyclerView.Adapter<PostViewHolder>() {
+class PostsAdapter(
+    private val onInteractionListener: OnInteractionListener,
+) : ListAdapter<Post, PostViewHolder>(PostDiffCallback()) {
 
-    var list = emptyList<Post>()
-        set(value) {
-            field = value
-            notifyDataSetChanged()
-        }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
         val binding = CardPostBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return PostViewHolder(binding, onLikeListener, onReplyListener)
+        return PostViewHolder(binding, onInteractionListener)
     }
 
     override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
-        val post = list[position]
+        val post = getItem(position)
         holder.bind(post)
     }
-
-    override fun getItemCount(): Int = list.size
 }
 
 class PostViewHolder(
     private val binding: CardPostBinding,
-    private val onLikeListener: OnLikeListener,
-    private val onReplyListener: OnReplyListener
+    private val onInteractionListener: OnInteractionListener,
 ) : RecyclerView.ViewHolder(binding.root) {
+
     fun bind(post: Post) {
         binding.apply {
             author.text = post.author
@@ -48,15 +50,42 @@ class PostViewHolder(
             like.setImageResource(
                 if (post.likedByMe) R.drawable.liked_24 else R.drawable.like
             )
+            menu.setOnClickListener {
+                PopupMenu(it.context, it).apply {
+                    inflate(R.menu.options_post)
+                    setOnMenuItemClickListener { item ->
+                        when (item.itemId) {
+                            R.id.remove ->{
+                                onInteractionListener.onRemove(post)
+                                true
+                            }
+                            R.id.edit -> {
+                                onInteractionListener.onEdit(post)
+                                true
+                            }
+                            else -> false
+                        }
+                    }
+                }.show()
+            }
             if (post.likedByMe) {
                 like.setImageResource(R.drawable.liked_24)
             }
             like.setOnClickListener{
-                onLikeListener(post)
+                onInteractionListener.onLike(post)
             }
             reply.setOnClickListener {
-                onReplyListener(post)
+                onInteractionListener.onReply(post)
             }
         }
+    }
+}
+class PostDiffCallback: DiffUtil.ItemCallback<Post>(){
+    override fun areItemsTheSame(oldItem: Post, newItem: Post): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areContentsTheSame(oldItem: Post, newItem: Post): Boolean {
+        return oldItem == newItem
     }
 }
